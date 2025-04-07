4687a4f0ee6209a91359e82101aef6b03bc3e3c0
kkv0id <kkv0id@outlook.com>
2025-04-07 12:38:50 +0000

    commit

--- luasrc/model/cbi/me909s/advance.lua
+++ luasrc/model/cbi/me909s/advance.lua
@@ -49,7 +49,6 @@ sys.exec("logger -t ME909s advance DEBUG " .. target_interface)
 local cur_imei
 if target_interface then
     cur_imei = uci:get("network", string.gsub(target_interface, "%s+", ""), "imei")
-    sys.exec("logger -t ME909s advance DEBUG " .. (cur_imei or "N/A"))
 end
 
 m = Map("me909s")
@@ -60,9 +59,7 @@ function ss.parse(self, section, novld)
     if cur_imei and new_imei ~= cur_imei then
         sys.exec("logger -t ME909s advance ss.parse DEBUG " .. new_imei) 
     end
-   
     sys.exec("logger -t ME909s advance ss.parse DEBUG " .. (luci.http.formvalue("cbid.me909s.1.imei1") == cur_imei and "true" or 'false'))
-
 end
 
 imei = ss:option(Value, "imei", translate("IMEI"))
@@ -75,10 +72,111 @@ imei.validate = function(self, value)
     return value
 end
 
-local save_btn = ss:option(Button, "save", translate("设置IMEI"))
-save_btn.inputtitle = translate("确定")
-save_btn.inputstyle = "apply"
-save_btn.write = function()
-    sys.exec("logger -t ME909s advance save_btn.write DEBUG " .. print_r(luci.http.formvalue()))
+local set_imei_btn = ss:option(Button, "set_imei", translate("设置IMEI"))
+set_imei_btn.inputtitle = translate("确定")
+set_imei_btn.inputstyle = "apply"
+
+local hex_to_bits = {["0"]="0000",["1"]="0001",["2"]="0010",["3"]="0011",["4"]="0100", ["5"]="0101",["6"]="0110",["7"]="0111",["8"]="1000",["9"]="1001",["a"]="1010",["b"]="1011",["c"]="1100",["d"]="1101",["e"]="1110",["f"]="1111"}
+
+local bits_to_hex = {}
+for k, v in pairs(hex_to_bits) do
+    bits_to_hex[v] = k:upper()
 end
+
+local function hex_bitwise_op(a, b, op)
+    local max_len = math.max(#a, #b)
+    -- 补零到左侧使长度相同，并转为小写
+    a = string.rep("0", max_len - #a) .. a:lower()
+    b = string.rep("0", max_len - #b) .. b:lower()
+    
+    local result = {}
+    for i = 1, max_len do
+        local c_a = a:sub(i, i)
+        local c_b = b:sub(i, i)
+        local bits_a = hex_to_bits[c_a]
+        local bits_b = hex_to_bits[c_b]
+        if not bits_a or not bits_b then
+            return nil, "invalid hex character"
+        end
+        local res_bits = ""
+        for j = 1, 4 do
+            local a_bit = bits_a:sub(j, j)
+            local b_bit = bits_b:sub(j, j)
+            local bit_result
+            if op == "AND" then
+                bit_result = (a_bit == "1" and b_bit == "1") and "1" or "0"
+            elseif op == "OR" then
+                bit_result = (a_bit == "1" or b_bit == "1") and "1" or "0"
+            else
+                return nil, "invalid operation"
+            end
+            res_bits = res_bits .. bit_result
+        end
+        local hex_char = bits_to_hex[res_bits]
+        if not hex_char then
+            return nil, "invalid resulting bits"
+        end
+        table.insert(result, hex_char)
+    end
+    -- 合并结果并去除前导零
+    local result_str = table.concat(result)
+    result_str = result_str:gsub("^0+", "")
+    if result_str == "" then
+        result_str = "0"
+    end
+    return result_str
+end
+
+function hex_and(a, b)
+    return hex_bitwise_op(a, b, "AND")
+end
+
+function hex_or(a, b)
+    return hex_bitwise_op(a, b, "OR")
+end
+
+ss = m:section(SimpleSection, "BAND", "设置BAND模块会重启")
+function ss.parse(self, section, novld)
+end
+
+local mode = ts:option(ListValue, "mode", translate("网络模式"))
+mode:value("030201", translate("preferLTE"))
+mode:value("0201", translate("preferUMTS"))
+mode:value("03", translate("LTE"))
+mode:value("02", translate("UMTS"))
+mode:value("01", translate("GSM"))
+mode:value("00", translate("AUTO"))
+
+local gms_umts_bands = {
+    { key = "GSM_DCS_1800", hex = "80" },
+    { key = "GSM_EGSM_900", hex = "100" },
+    { key = "GSM_PGSM_900", hex = "200" },
+    { key = "Band_1", hex = "400000" },
+    { key = "Band_8", hex = "2000000000000"}
+}
+local gms_umts_band = ss:option(MultiValue, "gms_umts_band", translate("GMS/UMTS频段"))
+for _, band in ipairs(gms_umts_bands) do
+    gms_umts_band:value(band.key, band.hex) 
+end
+
+local lte_bands = {
+    { key = "FDD1", hex = "1" },
+    { key = "FDD3", hex = "4" },
+    { key = "FDD5", hex = "10" },
+    { key = "FDD8", hex = "80" },
+    { key = "TDD34", hex = "200000000" },
+    { key = "TDD38", hex = "2000000000" },
+    { key = "TDD39", hex = "4000000000" },
+    { key = "TDD40", hex = "8000000000" },
+    { key = "TDD41", hex = "10000000000" }
+}
+local let_band = ss:option(MultiValue, "let_band", translate("LET频段"))
+for _, band in ipairs(lte_bands) do
+    let_band:value(band.key, band.hex) 
+end
+
+local set_band_btn = ss:option(Button, "set_band", translate("设置BAND"))
+set_band_btn.inputtitle = translate("确定")
+set_band_btn.inputstyle = "apply"
+
 return m
--- luasrc/model/cbi/me909s/setting.lua
+++ luasrc/model/cbi/me909s/setting.lua
@@ -5,16 +5,16 @@ m = Map("me909s", translate("设置"))
 ts = m:section(TypedSection, "setting")
 ts.addremove = false
 ts.anonymous = true
-sys.exec("logger -t ME909s DEBUG")
+
 local sim_val = sys.exec("cat /sys/class/gpio/sim_select/value")
-sys.exec("logger -t ME909s-setting-DEBUG '" .. sim_val .. "'")
+
 sim_slot = ts:option(ListValue, "sim_slot", translate("SIM卡切换"))
 sim_slot.default = sim_val
 sim_slot:value("1", translate("SIM"))
 sim_slot:value("0", translate("eSIM"))
 
 local usb_val = sys.exec("cat /sys/class/gpio/usb_power/value")
-sys.exec("logger -t ME909s-setting-DEBUG '" .. usb_val .. "'")
+
 usb_power = ts:option(ListValue, "usb_power", translate("USB开关"))
 usb_power.default = usb_val
 usb_power:value("1", translate("开"))
